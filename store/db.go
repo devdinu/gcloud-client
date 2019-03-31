4893514d5d283ab14a6d9dad28c98bb7c493d8ec
Dinesh Kumar <dinesh.kumar@go-jek.com>
2019-03-31 07:53:11 +0530

    search instances with prefix

--- store/db.go
+++ store/db.go
@@ -43,9 +43,25 @@ func (db DB) Save(ctx context.Context, instances <-chan gcloud.Instance, wg *syn
 			}
 			return b.Put([]byte(inst.Name), data.Bytes())
 		})
+		if err != nil {
+			return err
+		}
 	}
-	fmt.Println("stored all instances")
 	return nil
+}
+
+func (db DB) Search(ctx context.Context, pattern string) ([]gcloud.Instance, error) {
+	var insts []gcloud.Instance
+	db.View(func(tx *bolt.Tx) error {
+		c := tx.Bucket([]byte("wonderful-maps")).Cursor()
+		for k, v := c.Seek([]byte(pattern)); k != nil && bytes.HasPrefix(k, []byte(pattern)); k, v = c.Next() {
+			var data bytes.Buffer
+			gob.NewDecoder(&data).Decode(v)
+			fmt.Println("found: ", string(k), data.String())
+		}
+		return nil
+	})
+	return insts, nil
 }
 
 func NewDB() (DB, error) {
